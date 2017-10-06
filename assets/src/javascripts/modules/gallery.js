cf6fece20d876e2a688f0ce998ad1e5143151d36
Vladilen Tsvetkov <tini2n@gmail.com>
2017-10-06 16:15:07 +0300

    Content styles, single page

--- assets/src/javascripts/modules/gallery.js
+++ assets/src/javascripts/modules/gallery.js
@@ -1,17 +1,11 @@
 import $ from 'jquery';
 import Swiper from 'swiper';
 export default () => {
- //    let gallerySlider = new Swiper('.gallery-container', {
- //        grabCursor: true,
- //        loop: true,
- //        nextButton: '.gallery-container .swiper-button-next',
- //        prevButton: '.gallery-container .swiper-button-prev',
- //    });
-
- //    body.click(function(e) {
-	//     if (!$(e.target).closest('.gallery-container').length){
-	//     	galleryContainer.removeClass('gallery-popup-open');
-	//     }
-	// });
+    let gallerySlider = new Swiper('.gallery-widget', {
+        grabCursor: true,
+        loop: true,
+        nextButton: '.gallery-widget .swiper-button-next',
+        prevButton: '.gallery-widget .swiper-button-prev',
+    });
 }
 
