722d035d1c9632bb404efdb5c84f35ab992b2820
Vladilen Tsvetkov <tini2n@gmail.com>
2017-11-29 23:39:27 +0200

    Merge branch 'master' of /Users/tini2n/projects/vagrant/www/wordpress/wp-content/themes/colorrun with conflicts.

--- assets/src/javascripts/modules/registration.js
+++ assets/src/javascripts/modules/registration.js
@@ -5,7 +5,7 @@ export default () => {
     let participant = {};
     let $form = $('.registration-form');
     let $stepsArea = $('.steps-area');
-    let names = ['lastname', 'firstname', 'gender', 'email', 'dateofbirth', 'info[phone]', 'country', 'city', 'info[tshirt_size]', 'personal_data', 'event_rules'];
+    let names = ['lastname', 'firstname', 'gender', 'email','dateofbirth', 'info[phone]', 'country', 'city', 'info[tshirt_size]', 'personal_data', 'event_rules'];
 
     $('.edit-info').on('click', goToPrev);
 
@@ -14,11 +14,8 @@ export default () => {
 
         let fd = new FormData(this);
 
-        // for (const [key, value]  of fd.entries()) {
-        //     participant[key] = value;
-        // }
-        $form.find('input,select,textarea').each(function () {
-            let $input = $(this),
+        names.forEach(function (name) {
+            let $input = $form.find('[name="' + name + '"]'),
                 value;
 
             if ($input.attr('type') === 'radio')
@@ -26,9 +23,7 @@ export default () => {
             else
                 value = $input.val();
 
-            if (typeof value !== 'undefined') {
-                participant[$input.attr('name')] = value
-            }
+            participant[name] = value
         });
         $.post({
             url: '/wp-json/register/updateInfo', //TODO: set url from settings
