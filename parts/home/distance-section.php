7444370f8b17ebac5f213a49776d09d6eac8174a
Vitaly Nikolaiev <trilliput@gmail.com>
2018-01-18 21:35:03 +0200

    homepage empty section check

--- parts/home/distance-section.php
+++ parts/home/distance-section.php
@@ -5,28 +5,30 @@
  * @author     vitaly
  *
  */
-$col_classes = [
-	'dark',
-	'medium',
-	'light',
-]; ?>
-<section class="distance-section">
-    <div class="wrapper-outer">
-        <h2 class="section-title"><?php _e( 'Distances', 'colorrun' ) ?></h2>
-        <div class="wrapper-inner">
-            <div class="col image-container">
-				<?= Homepage::get_distances_image(); ?>
+$distances = Distance::get_current_distances();
+if ( $distances->have_posts() ):
+	$col_classes = [
+		'dark',
+		'medium',
+		'light',
+	]; ?>
+    <section class="distance-section">
+        <div class="wrapper-outer">
+            <h2 class="section-title"><?php _e( 'Distances', 'colorrun' ) ?></h2>
+            <div class="wrapper-inner">
+                <div class="col image-container">
+					<?= Homepage::get_distances_image(); ?>
+                </div>
+				<?php if ( $distances->have_posts() ):
+					while ( $distances->have_posts() ):
+						$distances->the_post(); ?>
+                        <div class="col <?= array_shift( $col_classes ) ?>">
+							<?php get_template_part( 'parts/home/distance/item' ) ?>
+                        </div>
+					<?php endwhile;
+				endif;
+				wp_reset_postdata(); ?>
             </div>
-			<?php $distances = Distance::get_current_distances();
-			if ( $distances->have_posts() ):
-				while ( $distances->have_posts() ):
-					$distances->the_post(); ?>
-                    <div class="col <?= array_shift( $col_classes ) ?>">
-						<?php get_template_part( 'parts/home/distance/item' ) ?>
-                    </div>
-				<?php endwhile;
-			endif;
-			wp_reset_postdata(); ?>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif;
--- parts/home/sponsors-section.php
+++ parts/home/sponsors-section.php
@@ -5,24 +5,21 @@
  * @author     vitaly
  *
  */ ?>
-<?php $sponsors = Sponsor::get_sponsors(); ?>
-<section class="sponsors-section">
-    <div class="wrapper-outer">
-    <div class="section-title">
-	    <?php _e( 'Sponsors and partners', 'colorrun' ) ?>
-    </div>
-<?php if ( $sponsors->have_posts() ): ?>
-        <div class="logos-container">
-	        <?php while ( $sponsors->have_posts() ):
-		        $sponsors->the_post();
-		        $url  = Sponsor::get_url();
-		        $type = Sponsor::get_type();
-		        ?>
-                <a href="<?= $url ?>" class="logo-item <?= $type ?>">
-	                <?php the_post_thumbnail( 'large' ) ?>
-                </a>
-	        <?php endwhile; ?>
+<?php $sponsors = Sponsor::get_sponsors();
+if ( $sponsors->have_posts() ): ?>
+    <section class="sponsors-section">
+        <div class="wrapper-outer">
+            <div class="section-title">
+				<?php _e( 'Sponsors and partners', 'colorrun' ) ?>
+            </div>
+            <div class="logos-container">
+				<?php while ( $sponsors->have_posts() ):
+					$sponsors->the_post(); ?>
+                    <a href="<?= Sponsor::get_url() ?>" class="logo-item <?= Sponsor::get_type() ?>">
+						<?php the_post_thumbnail( 'large' ) ?>
+                    </a>
+				<?php endwhile; ?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 <?php endif; ?>
